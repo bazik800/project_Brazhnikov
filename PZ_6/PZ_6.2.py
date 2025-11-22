e30951160440a5b9399a3afc4bd8aaedb05a7e89
bazik800 <81381614+bazik800@users.noreply.github.com>
2025-11-22 19:13:43 +0300

    Create PZ_6.2.py

---
+++ PZ_6/PZ_6.2.py
@@ -0,0 +1,13 @@
+#Дан список размера N. Найти максимальный из его локальных минимумов
+# (локальный минимум - это элемент, который меньше любого из своих соседей).
+import random
+n = int(input("Введите размер списка - "))
+s = [random.randint(0,100) for i in range(n)]
+d = []
+for i in range(n-1):
+    if s[i] < s[i-1] and s[i] < s[i+1]:
+        d.append((i,s[i]))
+
+print(s)
+print(d)
+print(max(d))
