25859e3870f1459161e9f4dc944759119b9af7d6
bazik800 <81381614+bazik800@users.noreply.github.com>
2026-03-28 19:01:42 +0300

    Update PZ_10.1.py

--- PZ_10/PZ_10.1.py
+++ PZ_10/PZ_10.1.py
@@ -3,52 +3,49 @@
 #Исходные данные
 #Кол-во элементов, произведение элементов, повторяющиеся элементы, кол-во повторяющихся элементов, элементы больше 5 увеличены в 2 раза
 import random
-a = open('text.txt', 'w')
-d = []
-d1 = []
-c=0
-c1=0
-for i in range(10):
-    n = random.randint(-10,10)  #Запись файла
-    a.write(str(n) + ' ')
-a.write('\n')
 
+a = open('text.txt', 'w')
+numbers = []
 for i in range(10):
-    c+=1
-a.write('Number of numbers: ')     #Количество элементов
-a.write(str(c))   
+    n = random.randint(-10, 10)
+    numbers.append(n) # Сохраняем в список, чтобы потом обработать
+    a.write(str(n) + ' ')
 a.close()
 
-s = open('text.txt', 'r+')
-cont = s.readline()
-numbers = [int(x) for x in cont.split()]
-total = sum(numbers)        #Сумма
-s.write('\nTotal sum: ')
-s.write(str(total))
-s.close()
+ss = open('text1.txt', 'w')
 
-q = open('text.txt', 'r+')
-cont1 = q.readline()
+# 1. Кол-во элементов
+c = 0
+for i in numbers:
+    c += 1
+ss.write('Number of numbers: ' + str(c) + '\n')
+
+# 2. Произведение элементов
+prod = 1
 for x in numbers:
-    if numbers.count(x) > 1 and x not in d:        #Нахождение повторов
+    prod *= x
+ss.write('Product: ' + str(prod) + '\n')
+
+# 3. Повторяющиеся элементы и их количество
+d = []
+for x in numbers:
+    if numbers.count(x) > 1 and x not in d:
         d.append(x)
-for i in d:         #Счетчик повторов
-    c1+=1
-q.write('\nRepeat: ')
-q.write(str(d))
-q.write('\nCount of repeat: ')
-q.write(str(c1))
-q.close()
 
-e = open('text.txt', 'r+')
-cont2 = e.readline()
-for i in range(len(numbers)):
-    if numbers[i] > 5:
-        numbers[i] = numbers[i] * 2         #Увелечение элементов
-e.write('\n<5: ')
-e.write(str(numbers))
+c1 = 0
+for i in d:
+    c1 += 1
 
+ss.write('Repeat: ' + str(d) + '\n')
+ss.write('Count of repeat: ' + str(c1) + '\n')
 
+# 4. Элементы больше 5 увеличены в 2 раза
+new_numbers = []
+for x in numbers:
+    if x > 5:
+        new_numbers.append(x * 2)
+    else:
+        new_numbers.append(x)
+ss.write('Elements > 5 doubled: ' + str(new_numbers) + '\n')
 
-
-
+ss.close()
