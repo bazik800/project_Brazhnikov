f959b86d26739faea9a77116bda5e884a0b6f1e3
bazik800 <81381614+bazik800@users.noreply.github.com>
2026-06-08 15:58:25 +0300

    Create PZ_13.1.py

---
+++ Регулярки/Регулярки_PZ/PZ_13.1.py
@@ -0,0 +1,23 @@
+with open('ip_address.txt', 'r') as f:
+    lines = f.readlines()
+
+with open('reserved_qualified.txt', 'w') as f1, open('reserved_others.txt', 'w') as f2:
+    count_first = 0
+    count_second = 0
+    for line in lines:
+        line = line.strip()
+        if not line:
+            continue
+        ip_part = line.split('/')[0]
+        octets = ip_part.split('.')
+        first_octet = int(octets[0])
+        second_octet = int(octets[1])
+        if first_octet != 0 and second_octet != 0:
+            f1.write(line + '\n')
+            count_first += 1
+        else:
+            f2.write(line + '\n')
+            count_second += 1
+
+print(f'В файле reserved_qualified.txt: {count_first} строк')
+print(f'В файле reserved_others.txt: {count_second} строк')
